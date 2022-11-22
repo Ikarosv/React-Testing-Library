30c01c4a52cd19b8b4b5fe11fbde79a96ce9d3ab
Ikarosv <ikarovieiraa@gmail.com>
2022-11-22 17:05:28 -0300

    test: adiciona o teste da página about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import About from '../pages/About';
+
+describe('Testando o component About', () => {
+  test('Testa se a página contém informações sobre a Pokédex', () => {
+    render(<About />);
+
+    expect(screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    })).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém 2 parágrafos com texto sobe a Pokédex', () => {
+    render(<About />);
+    expect(screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon')).toBeInTheDocument();
+
+    expect(screen.getByText('One can filter Pokémon by type, and see more details for each one of them')).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém uma imagem com a src correta', () => {
+    render(<About />);
+
+    const image = screen.getByAltText('Pokédex');
+
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
