18f4b39b6b62d8f75b9eb37bd2b5b951146bd470
Ikarosv <ikarovieiraa@gmail.com>
2022-11-22 21:04:22 -0300

    test: teste de not found em favoritepokemon

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+
+// beforeEach(() => {
+//   global.localStorage = jest.mock(window.localStorage);
+
+//   jest.spyOn(window.localStorage, 'setItem').mockImplementation((nameObj, value) => { localStorage[nameObj] = value; });
+//   jest.spyOn(window.localStorage, 'getItem').mockImplementation((nameObj) => localStorage[nameObj]);
+// });
+describe('Testando a página FavoritePokemon', () => {
+  test('Testa se a mensagem "No favorite pokemon found" é exibida caso não tenha nenhum pokémon favoritado', () => {
+    render(<FavoritePokemon />);
+
+    expect(screen.getByText('No favorite Pokémon found')).toBeInTheDocument();
+  });
+});
